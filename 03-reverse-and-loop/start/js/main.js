1dcad55668b1c6da3ce7d22050bdc63a0659f402
Simas <simonas@digitouch.lt>
2019-04-23 09:14:21 +0300

    using pins

--- 03-reverse-and-loop/start/js/main.js
+++ 03-reverse-and-loop/start/js/main.js
@@ -2,42 +2,18 @@ $(document).ready(function() {
   // Init Scroll Magic
   var controller = new ScrollMagic.Controller();
 
-  var ourScene = new ScrollMagic.Scene({
-    triggerElement: "#project01 img",
-    triggerHook: 0.9
-  })
-    .setClassToggle("#project01", "fade-in")
-    .addIndicators({
-      name: "fade scene",
-      colorTrigger: "black",
-      colorStart: "#75C695",
-      colorEnd: "pink"
+  $(".project").each(function() {
+    var ourScene = new ScrollMagic.Scene({
+      triggerElement: this.children[0],
+      triggerHook: 0.9
     })
-    .addTo(controller);
-
-  var ourScene2 = new ScrollMagic.Scene({
-    triggerElement: "#project02 img",
-    triggerHook: 0.9
-  })
-    .setClassToggle("#project02", "fade-in")
-    .addIndicators({
-      name: "fade scene 2",
-      colorTrigger: "black",
-      colorStart: "#75C695",
-      colorEnd: "pink"
-    })
-    .addTo(controller);
-
-  var ourScene3 = new ScrollMagic.Scene({
-    triggerElement: "#project03 img",
-    triggerHook: 0.9
-  })
-    .setClassToggle("#project03", "fade-in")
-    .addIndicators({
-      name: "fade scene 3",
-      colorTrigger: "black",
-      colorStart: "#75C695",
-      colorEnd: "pink"
-    })
-    .addTo(controller);
+      .setClassToggle(this, "fade-in")
+      .addIndicators({
+        name: "fade scene",
+        colorTrigger: "black",
+        colorStart: "#75C695",
+        colorEnd: "pink"
+      })
+      .addTo(controller);
+  });
 });
--- 04-simple-pin/start/js/main.js
+++ 04-simple-pin/start/js/main.js
@@ -1,67 +1,36 @@
-/*
-I like pinning #intro once
-with no duration
-not needing the pushFollowers
-property
-*/
+$(document).ready(function() {
+  // Init Scroll Magic
+  var controller = new ScrollMagic.Controller();
 
-$(document).ready(function(){
+  // pin intro
+  var pinIntroScene = new ScrollMagic.Scene({
+    triggerElement: "#intro",
+    triggerHook: 0,
+    duration: "30%"
+  })
+    .setPin("#intro", { pushFollowers: false })
+    .addTo(controller);
 
-	// Init ScrollMagic
-	var controller = new ScrollMagic.Controller();
+  // pin again
+  var pinIntroScene2 = new ScrollMagic.Scene({
+    triggerElement: "#project01",
+    triggerHook: 0.4
+  })
+    .setPin("#intro", { pushFollowers: false })
+    .addTo(controller);
 
-	// pin the intro
-	var pinIntroScene = new ScrollMagic.Scene({
-		triggerElement: '#intro',
-		triggerHook: 0,
-		duration: '30%' // Unpin after X% of scrolling
-	})
-	.setPin('#intro', {pushFollowers: false}) // Makes duration above work when above 0%
-	.addTo(controller);
-
-	// Repin #intro with a different trigger element
-	var pinIntroSceneTwo = new ScrollMagic.Scene({
-		triggerElement: '#project01',
-		triggerHook: 0.4
-	})
-	.setPin('#intro')
-	.addTo(controller);
-
-	// loop through each .project element
-	$('.project').each(function(){
-
-		// build a scene
-		var ourScene = new ScrollMagic.Scene({
-			triggerElement: this.children[0],
-			triggerHook: 0.9
-		})
-		.setClassToggle(this, 'fade-in') // add class to project01
-		.addIndicators({
-			name: 'fade scene',
-			colorTrigger: 'black',
-			colorStart: 'darkgreen',
-			colorEnd: 'blue'
-		}) // this requires a plugin
-		.addTo(controller);
-
-	});
-
+  $(".project").each(function() {
+    var ourScene = new ScrollMagic.Scene({
+      triggerElement: this.children[0],
+      triggerHook: 0.9
+    })
+      .setClassToggle(this, "fade-in")
+      .addIndicators({
+        name: "fade scene",
+        colorTrigger: "black",
+        colorStart: "#75C695",
+        colorEnd: "pink"
+      })
+      .addTo(controller);
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
